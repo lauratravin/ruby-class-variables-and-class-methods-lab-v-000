311aabbad95b52937cd6457e81aefa07cf68b2c9
Laura Messa Travin <laura.travin@gmail.com>
2018-05-16 20:56:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,16 +12,22 @@ attr_accessor :name, :artist, :genre
      @artist = a
      @genre = g
      @@count += 1
-    #  if @@artist_count.empty?
-    #    @@artist_count[a] = 1
-    #  else
+
        if @@artist_count.keys.include?(a)
          @@artist_count[a] += 1
        else
          @@artist_count[a] = 1
        end
-    #  end
+
+       if @@gerne_count.keys.include?(g)
+         @@gerne_count[g] += 1
+       else
+         @@gerne_count[g] = 1
+       end
+
    end
+
+
    def self.artist_count
     @@artist_count
    end
