2821c14de005c3e16509113a959935179484ee6b
Laura Messa Travin <laura.travin@gmail.com>
2018-05-16 20:33:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,15 @@ class Song
   @@count = 0
   @@genres = {}
   @@artists = {}
-  
+
    def initialized(s,a,g)
      @name = s
      @artist = a
      @genre = g
      @@count += 1
-     
-     
+
+
    end
-   
+
 
 end
